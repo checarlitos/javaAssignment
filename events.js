220e0e8307e7d6c394dd14a31d981d677e882c12
Carlos Beraun <cberaun2@cnm.edu>
2015-11-03 10:57:23 -0700

    added gitignore file and index.php file

---
+++ events.js
@@ -0,0 +1,56 @@
+/**
+ * changes background text on 1st par..
+ **/
+function mouseOver() {
+	document.getElementById("firstP").style.backgroundColor="red";
+}
+
+/**
+ * changes the background color when moused over
+ **/
+function mouseOver() {
+	document.getElementById("mouse").style.backgroundColor = "red";
+}
+
+/**
+ * changes background color when moused out
+ **/
+function mouseOut() {
+	document.getElementById("mouse").style.backgroundColor = "blue";
+}
+
+/**
+ * hides/shows the textarea when the checkbox is unchecked/checked
+ **/
+function showMoreInfo() {
+	if(document.getElementById("moreInfo").style.display === "block")
+	{
+		document.getElementById("moreInfo").style.display = "none";
+	} else {
+		document.getElementById("moreInfo").style.display = "block";
+	}
+}
+
+/**
+ * shows a modal window when clicked
+ **/
+function popUp() {
+	document.getElementById("signUp").style.display = "block";
+}
+
+/**
+ * closes the modal window when clicked
+ **/
+function closeSignUp() {
+	document.getElementById("signUp").style.display = "none";
+}
+
+/**
+ * generates a kitty based on the dimensions entered =^.^=
+ **/
+function resizeKitty() {
+	var dimension = document.getElementById("dimension").value;
+	var imageUrl  = "http://placekitten.com/g/" + dimension + "/" + dimension;
+	document.getElementById("dimensionValue").innerHTML = dimension;
+	document.getElementById("kitten").src = imageUrl;
+}
