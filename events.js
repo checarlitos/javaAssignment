3b1157967610d97dd0d3df35edc813296319edf5
Carlos Beraun <cberaun2@cnm.edu>
2015-11-03 13:22:19 -0700

    added gitignore file and index.php file

--- events.js
+++ events.js
@@ -2,7 +2,7 @@
  * changes the text on a click event
  **/
 function clickEvent() {
-	document.getElementById("click").innerHTML = "What di you do???? where did my paragraph go?.";
+	document.getElementById("click").innerHTML = "What did you do!?!?!?!? where did the paragraph go?.";
 }
 
 /**
